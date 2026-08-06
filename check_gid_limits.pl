04fd62f102d7f76f1aba4e6fcd743e92b09873aa
Chu Farszan (胡磺礵) <199257973+Farszan@users.noreply.github.com>
2026-08-06 18:46:49 +0900

    修正

--- check_gid_limits.pl
+++ check_gid_limits.pl
@@ -2,7 +2,7 @@
 
 use utf8;
 
-my $MAX_GLYPHS = 65528; # GID上限 (65535から安全マージンを除いた値)
+my $MAX_GLYPHS = 65528; # GID上限 (.notdef 等を除いた安全値)
 my $WORK_DIR   = "work";
 my $GLYPH_DIR  = "$WORK_DIR/glyph";
 my $codepoint_file = "$WORK_DIR/codepoint.txt";
@@ -30,7 +30,7 @@ foreach my $mode (@modes) {
   my %glyphlist = ();
   my %ivslist   = ();
 
-  # 1. codepoint.txt のパース（makefont.pl の条件判定を再現）
+  # 1. codepoint.txt のパース
   open my $fh, "<:utf8", $codepoint_file or die "Cannot open $codepoint_file: $!";
   while (<$fh>) {
     if ($arg eq "2") {
@@ -52,10 +52,27 @@ foreach my $mode (@modes) {
   }
   close $fh;
 
-  # 例外的に追加される基底文字
   $glyphlist{"u4e00"} = "u4e00" if ($arg eq "2" || $arg eq "3");[cite: 2]
 
-  # 2. IVS 統合（diff 判定による PUA 割り当て個数の試算）
+  # カウント初期化（.notdef 分で +1）
+  my $current_gid = 1;
+  my $exceeded_flag = 0;
+  my $overflow_file = "";
+
+  # A. 基底文字のカウント
+  my @base_keys = sort keys %glyphlist;
+  my $base_count = scalar(@base_keys);
+
+  foreach my $gname (@base_keys) {
+    $current_gid++;
+    if ($current_gid > $MAX_GLYPHS && !$exceeded_flag) {
+      $exceeded_flag = 1;
+      my $dir = "$GLYPH_DIR/" . substr($gname, 0, length($gname)-3) . "/" . substr($gname, 0, length($gname)-2);
+      $overflow_file = "$dir/$gname.svg (Base CodePoint: $gname)";
+    }
+  }
+
+  # B. IVS (PUA) のカウント
   my $pua_count = 0;
   if (scalar(keys(%ivslist)) > 0) {
     my %base_to_ivs = ();
@@ -64,7 +81,7 @@ foreach my $mode (@modes) {
       push @{$base_to_ivs{$temp[0]}}, $ucswithivs;
     }
 
-    foreach my $ucswithoutivs (keys %base_to_ivs) {
+    foreach my $ucswithoutivs (sort keys %base_to_ivs) {
       my %seen_svgs = ();
       my $base_dir = "$GLYPH_DIR/" . substr($ucswithoutivs, 0, length($ucswithoutivs)-3) . "/" . substr($ucswithoutivs, 0, length($ucswithoutivs)-2);
       my $base_svg = "$base_dir/$ucswithoutivs.svg";
@@ -74,10 +91,8 @@ foreach my $mode (@modes) {
         my $dir = "$GLYPH_DIR/" . substr($target_name, 0, length($target_name)-10) . "/" . substr($target_name, 0, length($target_name)-9);
         my $target_svg = "$dir/$ucswithivs.svg";
 
-        # A. 基底文字の SVG と一致するか
         next if (-e $target_svg && -e $base_svg && !`diff $target_svg $base_svg`);[cite: 2]
 
-        # B. 既出 IVS の SVG と一致するか
         my $matched = 0;
         foreach my $prev_svg (keys %seen_svgs) {
           if (-e $target_svg && !`diff $target_svg $prev_svg`) {[cite: 2]
@@ -86,32 +101,35 @@ foreach my $mode (@modes) {
           }
         }
 
-        # C. 新規字形なら PUA カウントを増やす
         unless ($matched) {
           $seen_svgs{$target_svg} = 1;
           $pua_count++;
+          $current_gid++;
+
+          if ($current_gid > $MAX_GLYPHS && !$exceeded_flag) {
+            $exceeded_flag = 1;
+            $overflow_file = "$target_svg (IVS Sequence: $ucswithivs)";
+          }
         }
       }
     }
   }
 
-  # 3. 概算 Total GID Calculation (.notdef = 1)
-  my $base_count = scalar(keys(%glyphlist));
-  my $total_gid  = $base_count + $pua_count + 1;
-
-  if ($total_gid > $MAX_GLYPHS) {
-    my $over = $total_gid - $MAX_GLYPHS;
-    printf(" [ERROR] %-12s : Total GID = %6d  (OVER BY %d!)\n", $name, $total_gid, $over);
+  # 結果の出力
+  if ($exceeded_flag) {
+    my $over = $current_gid - $MAX_GLYPHS;
+    printf("[ERROR] %-12s : Total GID = %6d (OVER BY %d!)\n", $name, $current_gid, $over);
+    print  "        └─ Exceeded limit at: $overflow_file\n";
     $has_error = 1;
   } else {
-    printf(" [OK]    %-12s : Total GID = %6d  (Base: %d, PUA: %d)\n", $name, $total_gid, $base_count, $pua_count);
+    printf("[OK]    %-12s : Total GID = %6d (Base: %d, PUA: %d)\n", $name, $current_gid, $base_count, $pua_count);
   }
 }
 
 print "--------------------------------------------------\n";
 
 if ($has_error) {
-  exit 1; # エラー終了（呼び出し元のシェルで検知）
+  exit 1;
 } else {
-  exit 0; # 正常終了
+  exit 0;
 }
