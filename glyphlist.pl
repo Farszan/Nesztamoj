8cbbf65701e4c7c519e479ee26d770850564b20b
Chu Farszan (胡磺礵) <199257973+Farszan@users.noreply.github.com>
2026-08-06 22:49:07 +0900

    glyphlistのuf8xx対応

--- glyphlist.pl
+++ glyphlist.pl
@@ -1,21 +1,36 @@
-﻿use utf8;
+﻿#!/usr/bin/perl
+
+use utf8;
 binmode STDOUT, ":utf8";
 
-@code = ();
-$blank = "<span style=\"background-color:#ccc;\">　</span>";
-$kana = "イ";
-$hangul = "가";
+my @code = ();
+my %ivs_map = (); # IVS (ue01xx / uf8xx 問わず一元管理)
+my $blank = "<span style=\"background-color:#ccc;\"> </span>";
+my $kana = "イ";
+my $hangul = "가";
 
-open FH, "<:utf8", "work/codepoint.txt";
+open FH, "<:utf8", "work/codepoint.txt" or die "Cannot open work/codepoint.txt: $!";
 while(<FH>){
-  $_ =~ m/^u([0-9a-f]+)\n$/;
-  $code[eval('0x'.$1)]++;
+  s/[\r\n]+$//;
+  
+  # A. 単一コードポイント (Base)
+  if($_ =~ m/^u([0-9a-f]+)$/i){
+    $code[hex($1)]++;
+  }
+  # B. IVSシーケンス (ue01xx および uf8xx / Rod)
+  elsif($_ =~ m/^u([0-9a-f]+)-u([0-9a-f]+)$/i){
+    my $base = lc($1);
+    my $vs   = uc($2); # 例: E0100, F800
+    push @{$ivs_map{$base}}, $vs;
+  }
 }
 close FH;
 
-print "<!DOCTYPE html><html><head><meta charset='utf8'><style>body{font-family:Nesztamoj,Nesztamoj2a,Nesztamoj2b,Nesztamoj3;}span:hover{color:red;background-color:yellow;}</style></head><body>";
+print "<!DOCTYPE html><html><head><meta charset='utf8'>";
+print "<style>body{font-family:Nesztamoj,Nesztamoj2a,Nesztamoj2b,Nesztamoj3;}span:hover{color:red;background-color:yellow;}</style></head><body>";
 print "<h1>Nesztamoj fonts</h1>font version: 2026-08-06<br><br>";
 
+# 1. 基底文字の一覧表示
 foreach(0x00 .. 0xfff){
   $high = $_;
   $buffer = "";
@@ -31,7 +46,7 @@ foreach(0x00 .. 0xfff){
       if($code == 0x302e || $code == 0x302f){
         $buffer .= $hangul;
       }
-      $buffer .= "<span title=\"".sprintf("U+%04X", $code)."\">".pack('U', $high * 256 + $low)."</span>";
+      $buffer .= "<span title=\"".sprintf("U+%04X", $code)."\">".pack('U', $code)."</span>";
       $count++;
     } else {
       $buffer .= $blank;
@@ -43,27 +58,29 @@ foreach(0x00 .. 0xfff){
   }
 }
 
-%ivd = ();
+print "<h2>IVS / Rod Variants List</h2>\n";
 
-open FH, "<:utf8", "work/IVD_Sequences.txt";
-while(<FH>){
-  if($_ =~ m/^([0-9A-F]+) (E01[0-9A-F]{2}); ([^;]+);/){
-    $ivd{lc($1)} .= $2.",";
-  }
-}
-close FH;
+# 2. IVS (IVD + Rod) の一覧表示
+foreach my $base (sort { hex($a) <=> hex($b) } keys(%ivs_map)){
+  my $base_dec = hex($base);
+  
+  # 基底文字 (U+XXXX) とその文字自体を表示
+  print pack('U', $base_dec) . sprintf(" (U+%04X) ", $base_dec);
 
-foreach(sort {hex($a) <=> hex($b)} keys(%ivd)){
-  $head = $_;
-  print pack('U', eval('0x'.$head)).sprintf("(U+%04X) ", eval('0x'.$head));
-  @temp = split(/,/, $ivd{$_});
-  %temp = ();
-  foreach(@temp){
-    $temp{$_}++;
+  # 重複排除と昇順ソート (E0100系, F800系順)
+  my %seen = ();
+  my @vs_list = grep { !$seen{$_}++ } @{$ivs_map{$base}};
+  @vs_list = sort { hex($a) <=> hex($b) } @vs_list;
+
+  foreach my $vs (@vs_list) {
+    my $vs_dec = hex($vs);
+    
+    # 基底文字 + 異体字セレクタ の結合文字を出力
+    print "<span title=\"" . sprintf("U+%04X U+%04X", $base_dec, $vs_dec) . "\">";
+    print pack('U', $base_dec) . pack('U', $vs_dec);
+    print "</span>";
   }
-  foreach(sort keys(%temp)){
-    print("<span title=\"".sprintf("U+%04X ", eval('0x'.$head))."U+$_\">".pack('U',eval('0x'.$head)).pack('U',eval('0x'.$_))."</span>");
-  }
-  print("<br>");
+  print "<br>\n";
 }
-print "</body></html>";
+
+print "</body></html>\n";
